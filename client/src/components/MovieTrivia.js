13ba7d1915ed7ce79290dd99250211cac5c36423
m3ia <meianatividad@gmail.com>
2022-09-12 19:27:30 -0700

    Start game menu

---
+++ client/src/components/MovieTrivia.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function MovieTrivia () {
+  return (
+    <div className="movie-trivia-container">
+      <p>this is movie trivia</p>
+    </div>
+  )
+}
