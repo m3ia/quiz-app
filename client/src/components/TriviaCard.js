0ffb341018cfdd173dc594182dc63d488a0c1a6b
m3ia <meianatividad@gmail.com>
2022-09-14 20:48:31 -0700

    WIP: show/hide icons based on result

--- client/src/components/TriviaCard.js
+++ client/src/components/TriviaCard.js
@@ -2,28 +2,32 @@ import React, {useEffect, useState} from 'react';
 
 export default function TriviaCard({ score, setScore, currCard, qIndex,  setQIndex, questionsData, createNewCard }) {
   const [showScore, setShowScore] = useState(false);
+  const [showIcons, setShowIcons] = useState(true);
+
+  const updateState =() => {
+    setShowIcons(true);
+  }
 
   const nextQuestion = (curr, corrA) => {
-    console.log('testing: ', curr, corrA)
-    console.log('score', score);
+    updateState();
     if (curr === corrA) {
       setScore(s => s + 1);
     }
     if (qIndex + 1 < questionsData.length) {
       setQIndex(i => i + 1);
-      createNewCard(questionsData[qIndex+1]);
+      setTimeout(() => createNewCard(questionsData[qIndex + 1]), 500);
+      setShowIcons(false);
     } else {
       setShowScore(true);
     }
-
   }
-  // useEffect(() => {
-
-  // }, currentQuestion)
+  useEffect(() => {
+    return;
+  }, [showIcons])
   return (
     <>
       {
-        showScore ? (<p>{score}</p>) : 
+        showScore ? (<h2>Score: {score}</h2>) : 
           (
             <>
               <p className="trivia-question">Question {qIndex+1}/{questionsData.length}: {currCard.question}</p>
@@ -34,7 +38,7 @@ export default function TriviaCard({ score, setScore, currCard, qIndex,  setQInd
                     onClick={() => nextQuestion(answer, currCard.correctAnswer)}
                     className="trivia-answer-btn"
                   >{answer}</button>
-                    <span className={`material-symbols-outlined ${answer === currCard.correctAnswer ? 'correct-answer-icon' : 'wrong-answer-icon'}`} key={`${answer}-icon`}> {answer === currCard.correctAnswer ? 'done' : 'close'} </span>
+                    <span className={`material-symbols-outlined ${answer === currCard.correctAnswer ? 'correct-answer-icon' : 'wrong-answer-icon'}`} key={`${answer}-icon`}> {showIcons && (answer === currCard.correctAnswer ? 'done' : 'close')} </span>
                   </p>
                 })}
             </>
